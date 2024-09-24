c01e31f01e9ff512add194a92d31bc3ec432d905
faznaa <faznaaharees@gmail.com>
2024-09-24 19:34:27 +0530

    ch

---
+++ src/lib/data.ts
@@ -0,0 +1,11 @@
+const heroData = {
+    title:"I'M SHEBY",
+    subtitle:"AI DEVELOPER & DATA SCIENTIST",
+    profile:'https://img.freepik.com/free-photo/side-view-ofserious-man_23-2148946213.jpg'
+}
+
+const welcomeSection = {
+    subtitle_top:'The road to freedom starts from here',
+    mainTitleWhite:'Welcome. Step into my world of',
+    mainTitleBlue:'AI Innovation.',
+}
